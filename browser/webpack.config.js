a8d3a946b223ea3350fce802e0bbae308506da65
Will Nelson <will@pleb.xyz>
2020-03-12 08:57:10 -0700

    Fix indentation

--- browser/webpack.config.js
+++ browser/webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+	},
 	mode: 'development',
 	plugins: [
 		new HtmlWebpackPlugin(),
