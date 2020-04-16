30b34d526cabc5498b707d0c453260fac813898c
Will Nelson <will@pleb.xyz>
2020-04-15 23:07:10 -0700

    Support self-identified signalling clients

--- signaler/src/main.rs
+++ signaler/src/main.rs
@@ -1,10 +1,7 @@
 use bytes;
 use dashmap::DashMap;
 use futures::StreamExt;
-use std::sync::{
-	Arc,
-	atomic::{AtomicUsize, Ordering},
-};
+use std::sync::Arc;
 use tokio::sync::mpsc;
 use warp::{
 	Filter,
@@ -19,12 +16,16 @@ use warp::{
 	},
 };
 
-static NEXT_CONN_ID: AtomicUsize = AtomicUsize::new(0);
-
-async fn ws_handler(ws: WebSocket, user_id: String, users: Users) {
+async fn ws_handler(ws: WebSocket, user_id: String, connection_id: String, users: Users) {
 	if !users.contains_key(&user_id) {
 		let conns: UserConnections = DashMap::new();
 		users.insert(user_id.clone(), conns);
+	}
+
+	let conns = users.get(&user_id).unwrap();
+	if conns.contains_key(&connection_id) {
+		let _ = ws.close().await;
+		return;
 	}
 
 	let (user_ws_tx, mut user_ws_rx) = ws.split();
@@ -32,24 +33,18 @@ async fn ws_handler(ws: WebSocket, user_id: String, users: Users) {
 	let (tx, rx) = mpsc::unbounded_channel();
 	tokio::task::spawn(rx.forward(user_ws_tx));
 
-	let conns = users.get(&user_id).unwrap();
-	let mut conn_id = NEXT_CONN_ID.fetch_add(1, Ordering::Relaxed);
-	while conns.contains_key(&conn_id) {
-		conn_id = NEXT_CONN_ID.fetch_add(1, Ordering::Relaxed);
-	}
-
-	conns.insert(conn_id, tx);
+	conns.insert(connection_id.clone(), tx);
 
 	while let Some(Ok(msg)) = user_ws_rx.next().await {
-		for conn in conns.value().iter().filter(|r#ref| *r#ref.key() != conn_id) {
+		for conn in conns.value().iter().filter(|r#ref| *r#ref.key() != connection_id) {
 			let _ = conn.send(Ok(msg.clone()));
 		}
 	}
 
-	conns.remove(&conn_id);
+	conns.remove(&connection_id);
 }
 
-type UserConnections = DashMap<usize, mpsc::UnboundedSender<Result<warp::ws::Message, warp::Error>>>;
+type UserConnections = DashMap<String, mpsc::UnboundedSender<Result<warp::ws::Message, warp::Error>>>;
 type Users = Arc<DashMap<String, UserConnections>>;
 
 #[tokio::main]
@@ -58,18 +53,18 @@ async fn main() {
 	let users: Users = Arc::new(users);
 	let users = warp::any().map(move || users.clone());
 
-	let ws = warp::path::param::<String>()
+	let ws = warp::path!(String / String)
 		.and(warp::ws())
 		.and(users.clone())
-		.map(|user_id: String, ws: Ws, users: Users| {
-			ws.on_upgrade(move |socket| ws_handler(socket, user_id, users))
+		.map(|user_id: String, connection_id: String, ws: Ws, users: Users| {
+			ws.on_upgrade(move |socket| ws_handler(socket, user_id, connection_id, users))
 		});
 
-	let post_client = warp::path!(String / usize)
+	let post_client = warp::path!(String / String)
 		.and(warp::post())
 		.and(warp::body::bytes())
 		.and(users.clone())
-		.map(|user_id: String, connection_id: usize, body: bytes::Bytes, users: Users| {
+		.map(|user_id: String, connection_id: String, body: bytes::Bytes, users: Users| {
 			users
 				.get(&user_id)
 				.as_ref()
@@ -83,7 +78,7 @@ async fn main() {
 		});
 
 	let get_clients = warp::path::param::<String>()
-		.and(warp::post())
+		.and(warp::get())
 		.and(users)
 		.map(|user_id: String, users: Users| {
 			match users.get(&user_id) {
